b4799880ed2c4b201c2b9e71065a1d704d441e8d
ahmady016 <ahmady09@gmail.com>
2024-07-09 02:29:40 +0300

    refactor global error handler to handle validationErrors

--- src/errorHandlerMiddleware.ts
+++ src/errorHandlerMiddleware.ts
@@ -1,30 +1,37 @@
 import { Request, Response, NextFunction } from 'express'
+import { ValidationError } from 'class-validator'
 
 export class ApiError extends Error {
 	statusCode?: number
+	validationErrors?: ValidationError[]
 	endpoint?: string
-	constructor(message: string, statusCode?: number, endpoint?: string) {
+	constructor(
+		message: string,
+		statusCode?: number,
+		validationErrors?: ValidationError[],
+		endpoint?: string
+	) {
 		super(message)
 		this.statusCode = statusCode || 500
 		this.endpoint = endpoint || process.env.BASE_URL
+		this.validationErrors = validationErrors || []
 	}
 }
 export default function errorHandlerMiddleware(
 	error: ApiError,
 	req: Request,
 	res: Response,
-	next: NextFunction
+	_: NextFunction
 ) {
-	if (error) {
-		const statusCode = error.statusCode ? error.statusCode : 500
-		const response: ApiError = {
-			name: 'ApiError',
-			statusCode: statusCode,
-			endpoint: req.url,
-			message: error.message,
-		}
-		console.error(response)
-		res.status(statusCode).send(response)
+	const statusCode = error.statusCode ? error.statusCode : 500
+	const errorName = error.name || 'ApiError'
+	const response: ApiError = {
+		name: errorName,
+		message: error.message,
+		statusCode: statusCode,
+		validationErrors: error.validationErrors,
+		endpoint: req.url,
 	}
-	next()
+	console.error(response)
+	res.status(statusCode).send(response)
 }
